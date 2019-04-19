882a902b54fd16d15ed890a6988c8f01efb1df00
Nely <nely@gmail.com>
2019-04-18 20:05:16 -0500

    ok

--- application/controllers/Administration.php
+++ application/controllers/Administration.php
@@ -83,6 +83,7 @@ class Administration extends Secure_area {
 			$data['status'] = "Opcion incorrecta";
 			$this->load->view('registrar_directores',$data);
 		}
+		redirect('/administration/listar_directores/');
 	}
 	public function delete_usuario($id_persona){
 		$this->Administration_model->delete_usuario($id_persona);
