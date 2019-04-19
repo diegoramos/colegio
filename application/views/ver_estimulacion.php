4b06211f2b5eff6c0368ce9c101425a77dc040e5
Nely <nely@gmail.com>
2019-04-18 19:46:10 -0500

    ok

--- application/views/ver_estimulacion.php
+++ application/views/ver_estimulacion.php
@@ -3,9 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<title>USUARIOS_SECRETARIA</title>
-
-	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/tabla.css">
 	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/tabla.css">
 </head>
 
 <body bgcolor="#222733">
@@ -30,7 +29,7 @@
 				<td> ". $row["APELLIDO"]. "</td>
 				<td> ". $row["USUARIO"]. "</td>
 				<td> ". $row["CONTRASENA"]. "</td>
-				<td><button type="button" class="btn btn-primary">Primary</button><button type="button" class="btn btn-danger">Danger</button><button type="button" class="btn btn-info">Info</button></td>
+				<td><button type="button" class="btn btn-primary">Editar</button><button type="button" class="btn btn-danger">Eliminar</button><button type="button" class="btn btn-info">Pagar</button></td>
 				</tr>
 			</tbody>
 		</table>
